36a2c97f743a03ae98a8ffdf2a60d15d18bf1f00
lanceorechovesky <IronYardPrograming@Josephs-MacBook-Pro.local>
2014-08-04 09:22:24 -0400

    im just playing with the git commands

---
+++ tiy_class/practice/learn_git.rb
@@ -0,0 +1,10 @@
+=begin
+use git status to check just that
+touch readme.md
+git init- makes it a git repostory- make sure its in the file you need it to be
+git add readme.md
+git remote add origin 
+git commit -am will add all files
+git add -A Will add all untracted files 
+git push -u origin
+=end 
